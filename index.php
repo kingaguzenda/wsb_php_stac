426daa2d79eee6e4e34b0c610e4b16591a3b076d
kingaguzenda <kingaguzenda09@gmail.com>
2023-03-01 10:59:51 +0100

    modyfikacja_plikow

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+$firstname = "Janusz";
+$surename = "Nowak";
+echo " Imiê i nazwisko: $firstname, $surename <br>";
+echo 'Imiê i nazwisko: $firstname, $surename <br>';
+
+echo <<< DATA
+<hr>
+Imiê: $firstname <br>
+Nazwisko: $surename <br>
+DATA;
